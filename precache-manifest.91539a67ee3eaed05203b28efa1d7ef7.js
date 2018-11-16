be1e1d98f8a8ce95fba4f4744138b939417c6569
ssomagani <seeta.somagani@gmail.com>
2018-11-16 07:50:19 -0500

    Updates

---
+++ precache-manifest.91539a67ee3eaed05203b28efa1d7ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef3123833c1182fac5c",
+    "url": "/react-tutorial/static/css/main.7c8084e1.chunk.css"
+  },
+  {
+    "revision": "0ef3123833c1182fac5c",
+    "url": "/react-tutorial/static/js/main.0ef31238.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-tutorial/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d218dd7cc2ecfc831c2caa70d3108340",
+    "url": "/react-tutorial/index.html"
+  }
+];
